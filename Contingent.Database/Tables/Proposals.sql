34cdb6611accdf7ddfa4e06f052abfbe954ec517
Ruslan Kutynko <kutynko@hotmail.com>
2015-10-13 03:00:54 +0300

    missed proposal table

---
+++ Contingent.Database/Tables/Proposals.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Proposals]
+(
+	Id uniqueidentifier not null,
+	Status int,
+	CreatedBy nvarchar(100) not null,
+	CreatedOn datetime2 not null,
+
+	constraint PK_Proposals primary key clustered (Id asc)
+)
