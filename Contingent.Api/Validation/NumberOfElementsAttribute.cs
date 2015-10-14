12e16f27fac1df61edbfb827a52dafa9c278f5a5
Ruslan Kutynko <kutynko@hotmail.com>
2015-10-15 01:54:33 +0300

    validation attribute

---
+++ Contingent.Api/Validation/NumberOfElementsAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Contingent.Api.Validation
+{
+    public sealed class NumberOfElementsAttribute : ValidationAttribute
+    {
+        public NumberOfElementsAttribute()
+        {
+            ErrorMessage = $"Collection must contain at least {AtLeast} element(s)";
+        }
+
+        public int AtLeast { get; set; }
+    }
+}
