e1cff707c2b5d9243f0bf3189e55eab05853b77d
Ruslan Kutynko <kutynko@hotmail.com>
2015-10-16 01:55:22 +0300

    orders table

---
+++ Contingent.Database/Tables/Orders.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] uniqueidentifier not null,
+	ProposalId uniqueidentifier not null,
+	RefNumber nvarchar(100) ,
+	SignedBy nvarchar(100),
+	SignedOn date not null,
+	Comments nvarchar(1000),
+	constraint PK_Orders primary key clustered (Id asc),
+	constraint FK_Orders_Proposals foreign key (ProposalId) references Proposals(Id)
+)
